d6a6c0e80b36f44d4d50e91e861f5fab878abac2
lustered <smellslikelavenders@gmail.com>
2020-11-29 01:44:30 -0500

    progress

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <bits/stdc++.h>
 
@@ -11,6 +12,14 @@ class term{
             this->coefficient = &coefficient;
             this->exponent = &exponent;
         }
+
+        std::string tv()
+        {
+            char buff[50];
+            snprintf(buff, sizeof(buff), "%dx^%d",*this->coefficient, *this->exponent);
+            std::string ret = buff;
+            return ret;
+        }
 };
 
 class node{
@@ -18,10 +27,11 @@ class node{
         term data;
         node* next;
 
-        node() : data(term(1,2)), next(NULL){}
+        node() : data(term(3,5)), next(NULL){}
 
         node(term exp) : data(exp), next(NULL){}
 
+        /* don't work */
         int* exp(){ return this->data.exponent;};
         int* coeff(){ return this->data.coefficient;};
 };
@@ -32,7 +42,8 @@ class polynomial{
 
         void addTerm(term* exp)
         {
-            node t = node(*exp); 
+            /* node t = node(*exp); */ 
+            node t = *exp; 
 
             if(this->head == NULL) 
             {
@@ -58,10 +69,33 @@ class polynomial{
                 }
             }
         }
+        std::string print(){
+            if(this->head == NULL)
+                return "empty polynomial";
+
+            node* t = this->head;
+            char buff[350];
+            std::string ret = "";
+
+            while(t != NULL)
+            {
+                snprintf(buff, sizeof(buff), "%dx^%d + ", *t->coeff(), *t->exp());
+                ret += buff;
+                t = t->next;
+            }
+
+            return ret;
+        }
 };
 
 int main()
 {
     
+    term t1 = term(3, 10);
+    std::cout << t1.tv() << std::endl;
+    /* node n1 = node(t1); */
+    node n1 = node();
+    /* std::cout << *n1.data.coefficient << " " << *n1.data.exponent << std::endl; */
+    std::cout << n1.data.tv() << std::endl;
     return 0;
 }
